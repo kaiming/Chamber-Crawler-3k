e8a2bb0acaf8eb5c14ca69650cf4cf3a0b2084be
kaiming <57194998+kaiming@users.noreply.github.com>
2020-08-06 05:15:43 -0400

    added Tile class interface and implementation

---
+++ Tile/tile.cc
@@ -0,0 +1,24 @@
+#include "tile.h"
+
+
+// constructor
+// NOTE: CAN CHANGE THIS TO TAKE A PAIR IF WE WANT
+Tile::Tile(char Type, int x, int y) : 
+    type {type},
+    coord {std::pair<int, int> {x, y}}
+{}
+
+
+// destructor
+Tile::~Tile() {}
+
+
+// accessors
+char Tile::getType() const {
+    return type;
+}
+
+
+std::pair<int, int> Tile::getCoord() const {
+    return coord;
+}
--- Tile/tile.h
+++ Tile/tile.h
@@ -1,8 +1,20 @@
 #ifndef __TILE_H__
 #define __TILE_H__
+#include <utility>
 
 class Tile {
+    char type;
+    std::pair<int, int> coord;
 
+  public:
+    // constructor
+    Tile(char type, int x, int y);
+    // destructor (does nothing)
+    virtual ~Tile() = 0;
+
+    // accessors
+    char getType() const;
+    std::pair<int, int> getCoord() const;
 };
 
 #endif
