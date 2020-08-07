72ad45d3a582e28fc20ca26b435081f4e92832ed
Kaiming Qiu <57194998+kaiming@users.noreply.github.com>
2020-08-07 00:07:58 -0400

    Update Factory/nonwalkablefactory.cc
    Co-authored-by: Allen Lu <32047303+super-nimbus@users.noreply.github.com>

--- Factory/nonwalkablefactory.cc
+++ Factory/nonwalkablefactory.cc
@@ -3,6 +3,6 @@
 
 
 // makeTile creates a tile that players and enemies are not able to walk on
-std::shared_ptr<Tile> makeTile(char type, int x, int y, std::shared_ptr<Character> occupant, std::shared_ptr<Potion> potion, std::vector<std::shared_ptr<Gold>> goldStash, bool exit) {
+std::shared_ptr<Tile> NonWalkableFactory::makeTile(char type, int x, int y, std::shared_ptr<Character> occupant, std::shared_ptr<Potion> potion, std::vector<std::shared_ptr<Gold>> goldStash, bool exit) {
     return std::make_shared<Tile>(type, x, y);
 }
