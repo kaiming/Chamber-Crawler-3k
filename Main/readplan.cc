8529c13b7c83937354b9b64ebd502bcefef2281b
kaiming <57194998+kaiming@users.noreply.github.com>
2020-08-09 01:13:45 -0400

    changed readFloorPlan

--- Main/readplan.cc
+++ Main/readplan.cc
@@ -390,8 +390,11 @@ void readFloorPlan(std::istream& in, std::vector<std::vector<std::vector<std::sh
                     {
                         // player spawn
                         auto tempPlayer {std::make_shared<Player>()}; // NOTE: GAVE PLAYER A DEFAULT CONSTRUCTOR INITIALIZES FIELDS TO 0 SINCE THE BOARD CONSTRUCTOR WILL REASSIGN THE OCCUPANT FIELD FOR PLAYER
+                        std::shared_ptr<Character> tempCharacter;
+                        tempCharacter = tempPlayer;
                         auto tempGold = std::vector<std::shared_ptr<Gold>> {};
-                        auto tempTile {tileType->makeTile(type, colNum, rowNum, tempPlayer, nullptr, tempGold, 0)};
+                        auto tempPotion = std::make_shared<Potion>(nullptr);
+                        auto tempTile {tileType->makeTile(type, colNum, rowNum, tempPlayer, tempPotion, tempGold, false)};
                         
                         // if the current floor has more than 1 @ symbol or none at all ------------------------------
                         if (floorNum != static_cast<int>(playerSpawns.size())) {
