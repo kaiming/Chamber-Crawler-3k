006d6b26df7e6366e9ebf6fbffb4b42d0549ec4b
kaiming <57194998+kaiming@users.noreply.github.com>
2020-08-14 17:54:21 -0400

    added const to some parameters

--- Factory/walkablefactory.cc
+++ Factory/walkablefactory.cc
@@ -4,6 +4,6 @@
 
 
 // WalkableFactory::makeTile(...) makes a WalkableTile with either default fields or specified ones
-std::shared_ptr<Tile> WalkableFactory::makeTile(char type, int x, int y, std::shared_ptr<Character> occupant, std::shared_ptr<Potion> potion, std::vector<std::shared_ptr<Gold>> goldStash, bool exit) {
+std::shared_ptr<Tile> WalkableFactory::makeTile(const char type, int x, int y, std::shared_ptr<Character> occupant, std::shared_ptr<Potion> potion, std::vector<std::shared_ptr<Gold>> goldStash, bool exit) {
     return std::make_shared<WalkableTile>(type, x, y, occupant, potion, goldStash, exit);
 }
