33106f8cf0fc762a9de0d5d37428c99ecb575256
Allen Lu <allenlu@live.ca>
2020-08-15 05:00:18 -0400

    revert

--- Board/board.cc
+++ Board/board.cc
@@ -781,6 +781,7 @@ std::string Board::moveEnemies() {
 
             // Swap Enemy pointers
             if (destination != nullptr) {
+                std::cout << destination << ": destination ptr" << std::endl;
                 (*it) = (*it)->move(destination);
             }
 
