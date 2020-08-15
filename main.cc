01e9bfd117b1ba1707728138bb48b78cbff1c7da
Allen Lu <allenlu@live.ca>
2020-08-15 00:46:15 -0400

    added new lines pt2

--- main.cc
+++ main.cc
@@ -148,6 +148,7 @@ int main(int argc, char* argv[]) {
                 td.drawFloor(std::cout, b, message);
             } else if (cmd == "r") {
                 // Restart game
+                std::cout << "Resetting the game..." << std::endl;
                 restart = true;
                 winner = 0;
 
