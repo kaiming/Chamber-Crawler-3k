9ef77e6e9b51441ffc5ad82aafa1322c9ed3165c
Allen Lu <allenlu@live.ca>
2020-08-15 05:07:57 -0400

    Revert "major fixes to generation"
    This reverts commit 24f285a1751d3c2ccd5666933ff6d86269751c0f.
    
    reverting

--- Board/board.cc
+++ Board/board.cc
@@ -497,13 +497,6 @@ std::string Board::attackEnemy(std::string direction) {
                             continue;
                         }
 
-                        // Check for occupants
-                        if (destination->getOccupant() != nullptr) {
-                            // Occupied by Character, loop again
-                            destination = nullptr;
-                            continue;
-                        }
-
                         // Check for gold
                         if (destination->getGold() != nullptr) {
                             // Occupied by gold, loop again
@@ -634,13 +627,6 @@ std::string Board::moveEnemies() {
                         continue;
                     }
 
-                    // Check for occupants
-                    if (destination->getOccupant() != nullptr) {
-                        // Occupied by Character, loop again
-                        destination = nullptr;
-                        continue;
-                    }
-
                     // Check for gold
                     if (destination->getGold() != nullptr) {
                         // Occupied by gold, loop again
@@ -738,13 +724,6 @@ std::string Board::moveEnemies() {
 
                 // Check in chamber (not a doorway or hallway)
                 if (destination->getRoom() < 0) {
-                    destination = nullptr;
-                    continue;
-                }
-                
-                // Check for occupants
-                if (destination->getOccupant() != nullptr) {
-                    // Occupied by Character, loop again
                     destination = nullptr;
                     continue;
                 }
@@ -900,7 +879,7 @@ void Board::generateFloor() {
 
     tile = std::rand() % static_cast<int>(chambers[chamber].size());
 
-    // Insert exit stairs
+        // Insert exit stairs
     chambers[chamber][tile]->setExit(true);
 
 
@@ -925,23 +904,13 @@ void Board::generateFloor() {
         }
 
         // Generate location (make sure not occupied)
-        int i = 0;
         do {
-            // Infinite loop protection
-            if (i > 250) {
-                break;
-            } else {
-                i++;
-            }
-
             chamber = std::rand() % static_cast<int>(chambers.size());
             tile = std::rand() % static_cast<int>(chambers[chamber].size());
-        } while (chambers[chamber][tile]->getPotion() || chambers[chamber][tile]->isExit() || chambers[chamber][tile]->getOccupant());
+        } while (chambers[chamber][tile]->getPotion());
 
         // Place type at location
-        if (!chambers[chamber][tile]->getPotion() && !chambers[chamber][tile]->isExit() && !chambers[chamber][tile]->getOccupant()) {
-            chambers[chamber][tile]->setPotion(temp);
-        }
+        chambers[chamber][tile]->setPotion(temp);
     }
 
 
@@ -960,24 +929,14 @@ void Board::generateFloor() {
         }
 
         // Generate location (make sure not occupied)
-        int i = 0;
         do {
-            // Infinite loop protection
-            if (i > 250) {
-                break;
-            } else {
-                i++;
-            }
-
             chamber = std::rand() % static_cast<int>(chambers.size());
             tile = std::rand() % static_cast<int>(chambers[chamber].size());
-        } while (chambers[chamber][tile]->getGold() || chambers[chamber][tile]->getPotion() || chambers[chamber][tile]->isExit() || chambers[chamber][tile]->getOccupant()); 
+        } while (chambers[chamber][tile]->getPotion() || chambers[chamber][tile]->getGold()); // DLC EXTENSION HERE: if gold can stack, remove second if clause
         
 
         // Place type at location
-        if (!chambers[chamber][tile]->getGold() && !chambers[chamber][tile]->getPotion() && !chambers[chamber][tile]->isExit() && !chambers[chamber][tile]->getOccupant()) {            
-            chambers[chamber][tile]->setGold(temp);
-        }
+        chambers[chamber][tile]->setGold(temp);
 
         // If dragonHoard type, place Dragon around it
         if (type == 7) {
@@ -988,15 +947,7 @@ void Board::generateFloor() {
             std::shared_ptr<WalkableTile> destination = nullptr;
 
             // Generate direction around DragonHoard
-            int i = 0;
             do {
-                // Infinite loop protection
-                if (i > 250) {
-                    break;
-                } else {
-                    i++;
-                }
-
                 int rng = std::rand() % 8;
 
                 if (rng == 0) {
@@ -1016,13 +967,11 @@ void Board::generateFloor() {
                 } else if (rng == 7) {
                     destination = validDest(chambers[chamber][tile], "sw");
                 }
-            } while (chambers[chamber][tile]->getGold() || chambers[chamber][tile]->getPotion() || chambers[chamber][tile]->isExit() || chambers[chamber][tile]->getOccupant());
+            } while (destination == nullptr);
 
             // Place dragon at location and store in DragonHoard
-            if (!chambers[chamber][tile]->getGold() && !chambers[chamber][tile]->getPotion() && !chambers[chamber][tile]->isExit() && !chambers[chamber][tile]->getOccupant()) {
-                destination->setOccupant(std::dynamic_pointer_cast<DragonHoard>(temp)->getDragon());
-                std::dynamic_pointer_cast<DragonHoard>(temp)->setDragonTile(destination);
-            }
+            destination->setOccupant(std::dynamic_pointer_cast<DragonHoard>(temp)->getDragon());
+            std::dynamic_pointer_cast<DragonHoard>(temp)->setDragonTile(destination);
         }
     }
 
@@ -1048,24 +997,14 @@ void Board::generateFloor() {
         }
 
         // Generate location (make sure not occupied)
-        int i = 0;
         do {
-            // Infinite loop protection
-            if (i > 250) {
-                break;
-            } else {
-                i++;
-            }
-
             chamber = std::rand() % static_cast<int>(chambers.size());
             tile = std::rand() % static_cast<int>(chambers[chamber].size());
-        } while (chambers[chamber][tile]->getGold() || chambers[chamber][tile]->getPotion() || chambers[chamber][tile]->isExit() || chambers[chamber][tile]->getOccupant());
+        } while (chambers[chamber][tile]->getPotion() != nullptr || chambers[chamber][tile]->getGold() != nullptr || chambers[chamber][tile]->getOccupant() != nullptr || chambers[chamber][tile]->isExit());
 
         // Place type at location
-        if (!chambers[chamber][tile]->getGold() && !chambers[chamber][tile]->getPotion() && !chambers[chamber][tile]->isExit() && !chambers[chamber][tile]->getOccupant()) {
-            chambers[chamber][tile]->setOccupant(temp);
-            enemies[floorNum-1].emplace_back(chambers[chamber][tile]);
-        }
+        chambers[chamber][tile]->setOccupant(temp);
+        enemies[floorNum-1].emplace_back(chambers[chamber][tile]);
     }
 }
 
