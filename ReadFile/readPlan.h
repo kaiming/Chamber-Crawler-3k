06a2d5b362ba9afc66deb8febcc864beb7b54e89
Allen Lu <allenlu@live.ca>
2020-08-15 01:09:45 -0400

    major changes -> converting dragonhoard and enemies to 2-D vectors pt2

--- ReadFile/readPlan.h
+++ ReadFile/readPlan.h
@@ -7,6 +7,6 @@
 class Tile;
 class WalkableTile;
 
-void readFloorPlan(std::istream& in, std::vector<std::vector<std::vector<std::shared_ptr<Tile>>>>& floors, std::vector<std::shared_ptr<WalkableTile>>& playerSpawns, std::vector<std::shared_ptr<WalkableTile>>& enemies, std::vector<std::shared_ptr<WalkableTile>>& dragonHoards, std::vector<bool>& filled);
+void readFloorPlan(std::istream& in, std::vector<std::vector<std::vector<std::shared_ptr<Tile>>>>& floors, std::vector<std::shared_ptr<WalkableTile>>& playerSpawns, std::vector<std::vector<std::shared_ptr<WalkableTile>>>& enemies, std::vector<std::vector<std::shared_ptr<WalkableTile>>>& dragonHoards, std::vector<bool>& filled);
 
 #endif
